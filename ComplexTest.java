a807ace9f7da785ad9d884c171d7a47a0a07cbbf
Bartek <bartosz.dadas@wp.pl>
2021-03-23 21:01:37 +0100

    function initializer

---
+++ ComplexTest.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+public  class ComplexTest {
+    public static void main(String[] args) {
+//        FootConverter newFoot = new FootConverter();
+//        System.out.println(newFoot.convertTometr());
+        int[] fib = new int[10];
+        FibonacciArray.fibFill(fib);
+        System.out.println(Arrays.toString(fib));
+//        Car.RaceCar Lamborghini = new Car.RaceCar();
+//        Car.checkAndTank(Type.Petrol,17);
+    }
+}
