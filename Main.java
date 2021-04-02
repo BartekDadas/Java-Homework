35a381cc3d176f5a1931f6966d538f097a27077e
Bartek <bartosz.dadas@wp.pl>
2021-04-02 09:12:02 +0200

    add ATM app

--- Main.java
+++ Main.java
@@ -1,12 +1,6 @@
-import java.util.Arrays;
 public  class  Main {
     public static void main(String[] args) {
-//        System.out.println("Input:");
-//        FootConverter feet = new FootConverter();
-//        System.out.println("Equals:");
-//        System.out.println(feet.checkInput());
-//        System.out.println(Arrays.toString(FibonacciArray.fibonacciArray(30)));
-//        RaceCar Lamborghini = new RaceCar();
-//        System.out.println(Lamborghini.checkType(Type.Petrol, 800));
+        ATM polishATM = new ATM();
+        polishATM.login();
     }
 }
